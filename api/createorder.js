1c776b4f797039d732e20157897ec7a02cbf5cec
stylehubstudio <deepakmathur9888813@gmail.com>
2026-01-16 00:45:47 +0530

    roza

--- api/createorder.js
+++ api/createorder.js
@@ -7,8 +7,8 @@ export default async function handler(req, res) {
 
   try {
     const razorpay = new Razorpay({
-      key_id: process.env.RAZORPAY_KEY_ID,
-      key_secret: process.env.RAZORPAY_KEY_SECRET,
+      key_id: process.env.REACT_APP_RAZORPAY_KEY_ID,
+      key_secret: process.env.REACT_APP_RAZORPAY_KEY_SECRET,
     });
 
     const { amount } = req.body;
